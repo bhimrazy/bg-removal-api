3041ecca6e7e5688506d189070f9db3e25abe10e
Bhimraj Yadav <bhimrajyadav977@gmail.com>
2024-08-30 18:47:15 +0000

    chore: Refactor server.py for improved image handling

--- server.py
+++ server.py
@@ -1,25 +1,23 @@
 from io import BytesIO
 
 import litserve as ls
-from fastapi import Response
+from fastapi import Response, UploadFile
 from PIL import Image
 from transformers import pipeline
 
 
 class BackgroundRemovalAPI(ls.LitAPI):
     def setup(self, device):
-        # Initialize the image segmentation model
         self.segmentation_model = pipeline(
             "image-segmentation", model="briaai/RMBG-1.4", trust_remote_code=True
         )
 
-    def decode_request(self, request):
+    def decode_request(self, request: UploadFile):
         # Extract image file from the request
-        return request["content"].file
+        return Image.open(request.file)
 
-    def predict(self, image_file):
+    def predict(self, image):
         # Perform background removal on the image
-        image = Image.open(image_file)
         result = self.segmentation_model(image)
         return result
 
